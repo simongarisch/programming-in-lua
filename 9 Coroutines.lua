2ee747ca5fd5f1a3f104a4675697c2b405f086c9
simongarisch <gatman946@gmail.com>
2018-07-27 17:43:36 +1000

    Working on coroutines

--- 9 Coroutines.lua
+++ 9 Coroutines.lua
@@ -98,3 +98,79 @@ f = filter(p)
 consumer(p) -- 'a' so we get the first letter
 
 print(coroutine.status(p)) -- suspended
+
+-- more on Lua coroutines from here... http://lua-users.org/wiki/CoroutinesTutorial
+-- Coroutines in Lua are not operating system threads or processes
+-- Only one coroutine ever runs at a time, and it runs until it activates another coroutine, or yields (returns to the coroutine that invoked it).
+--  Coroutines are a way to express multiple cooperating threads of control in a convenient and natural way,
+-- but do not execute in parallel, and thus gain no performance benefit from multiple CPU's.
+-- However, since coroutines switch much faster than operating system threads and do not typically require complex and sometimes expensive locking
+-- mechanisms, using coroutines is typically faster than the equivalent program using full OS threads.
+
+-- simple usage
+function foo()
+  print("foo", 1)
+  coroutine.yield()
+  print("foo", 2)
+end
+
+co = coroutine.create(foo)
+print(type(co)) -- thread
+print(coroutine.status(co)) -- suspended
+-- The state suspended means that the thread is alive, and as you would expect, not doing anything.
+-- To start the thread we use the coroutine.resume() function.
+coroutine.resume(co) -- foo 1
+print(coroutine.status(co)) -- suspended
+coroutine.resume(co) -- foo 2
+print(coroutine.status(co)) -- dead
+print(string.rep("*", 30))
+
+-- a more complicated example
+function odd(x)
+  print("A: odd", x)
+  coroutine.yield(x)
+  print("B: odd", x)
+end
+
+function even(x)
+  print("C: even", x)
+  if x==2 then return x end
+  print("D: even", x)
+end
+
+co = coroutine.create(
+  function (x)
+    for i=1,x do
+      if i==3 then coroutine.yield(-1) end
+      if i % 2 == 0 then even(i) else odd(i) end
+    end
+  end
+)
+
+count = 1
+while coroutine.status(co) ~= "dead" do
+  print("---", count); count = count + 1
+  errorfree, value = coroutine.resume(co, 5) -- resume / start co passing in x = 5
+  print("E: errorfree, value, status", errorfree, value, coroutine.status(co))
+end
+--[[
+--- 1
+A: odd  1
+E: errorfree, value, status true  1 suspended
+--- 2
+B: odd  1
+C: even 2
+E: errorfree, value, status true  -1  suspended
+--- 3
+A: odd  3
+E: errorfree, value, status true  3 suspended
+--- 4
+B: odd  3
+C: even 4
+D: even 4
+A: odd  5
+E: errorfree, value, status true  5 suspended
+--- 5
+B: odd  5
+E: errorfree, value, status true  nil dead
+]]
