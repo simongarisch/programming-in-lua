4e3e3b145c7d26872ed5f4487b59234c46e5d0e0
simongarisch <gatman946@gmail.com>
2018-08-06 08:20:28 +1000

    Working with the global envt

--- 14 The Environment.lua
+++ 14 The Environment.lua
@@ -5,7 +5,80 @@
 for k,v in pairs(_G) do
   print(k, v)
 end
+print(type(_G)) -- table
+print(string.rep("*", 30))
+print("")
 
--- for example
-version = _G[_VERSION]
-print(version) -- 
+-- because the environment is a regular table, you can
+-- simply index it by the desired key (the variable name)
+print(_G._VERSION)  -- Lua 5.1
+_G.myVarName = 42
+print(_G.myVarName) -- 42
+
+-- because Lua keeps its global variables in a regular table
+-- we can use metatables to change its behavior when accessing
+-- global variables
+setmetatable(_G, {
+  __newindex = function(_, n)
+    error("attempt to write to undeclared variable " .. n, 2)
+  end,
+  __index = function(_, n)
+    error("attempt to read undeclared variable " .. n, 2)
+  end
+})
+
+local success, result = pcall(
+  function()
+    return 3
+  end
+)
+print(success, result) -- true 3
+
+-- now any attempt to write to _G will raise an error
+local success, result = pcall(
+  function ()
+    _G.someVar = 42
+  end
+)
+print(success, result) 
+-- false, attempt to write to undeclared variable someVar
+
+success, result = pcall(
+  function()
+    return _G.someVar
+  end
+)
+print(success, result)
+-- false, attempt to read undeclared variable someVar
+
+-- non global environments
+local declaredNames = {}
+setmetatable(_G, {
+  __newindex = function(t, n, v)
+    if not declaredNames[n] then
+      local w = debug.getinfo(2, "S").what
+      if w ~= "main" and w ~= "C" then
+        error("attempt to write to undeclared variable " ..n, 2)
+      end
+      declaredNames[n] = true
+    end
+    rawset(t, n, v) -- do the actual set
+  end,
+  
+  __index = function(_, n)
+    if not declaredNames[n] then
+      error("attempt to read undeclared variable " ..n, 2)
+    else
+      return nil
+    end
+  end,
+})
+
+a = 1
+setfenv(1, {g = _G})
+g.print(a)
+g.print(g.a)
+
+setfenv(1, {_G = _G})
+_G.print(a)    -- nil
+_G.print(_G.a) -- 1
