2a5ad29c6d058b8b4e4984001d68bf222f11a7f5
U-MAGELLAN\simon.garisch <simon.garisch@MAM-LT238.magellan.sydney>
2018-09-04 18:12:46 +1000

    Added lua color names example

--- 25 Extending your application/names.c
+++ 25 Extending your application/names.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,10 +20,31 @@ struct ColorTable{
   {NULL, 0, 0, 0} /* sentinel */
 };
 
+// error function defined in 24.1
+void error (lua_State *L, const char *fmt, ...) {
+  va_list argp;
+  va_start(argp, fmt);
+  vfprintf(stderr, fmt, argp);
+  va_end(argp);
+  lua_close(L);
+  exit(EXIT_FAILURE);
+}
 
+// Lua 5.1 also offers a specialized version of lua_settable
+// for string keys, called lua_setfield p.g. 233
 void setfield (lua_State *L, const char *index, int value) {
   lua_pushnumber(L, (double)value/MAX_COLOR);
   lua_setfield(L, -2, index);
+}
+
+// defines a single color
+void setcolor (lua_State *L, struct ColorTable *ct) {
+  lua_newtable(L); /* creates a table */
+  // create index and value lua table pairs with setfield  
+  setfield(L, "r", ct->red);   /* table.r = ct->r */
+  setfield(L, "g", ct->green); /* table.g = ct->g */
+  setfield(L, "b", ct->blue);  /* table.b = ct->b */
+  lua_setglobal(L, ct->name);  /* ’name’ = table */
 }
 
 /* assume that table is on the stack top */
@@ -37,32 +59,52 @@ int getfield (lua_State *L, const char *key) {
   return result;
 }
 
-
-void setcolor (lua_State *L, struct ColorTable *ct) {
-  lua_newtable(L); /* creates a table */
-  setfield(L, "r", ct->red); /* table.r = ct->r */
-  setfield(L, "g", ct->green); /* table.g = ct->g */
-  setfield(L, "b", ct->blue); /* table.b = ct->b */
-  lua_setglobal(L, ct->name); /* ’name’ = table */
-}
-
 int main (void) {
   lua_State *L = luaL_newstate(); /* opens Lua */
   luaL_openlibs(L);               /* opens the standard libraries */
  
-  setcolor(L, colortable);
-  
-  unsigned char r, g, b;
-  int is_table = lua_istable(L,-1);
-  printf("Do we have a table? %d\n", is_table);
-  
-  if(lua_istable(L,-1)){
-    r = getfield(L, "r");
-	g = getfield(L, "g");
-	b = getfield(L, "b");
+  // register the different colors with Lua
+  int i = 0;
+  while(colortable[i].name != NULL){
+    setcolor(L, &colortable[i++]);	  
   }
+  
+  // run the configuration file
+  const char *fname = "get_background.lua";
+  if (luaL_loadfile(L, fname) || lua_pcall(L, 0, 0, 0)){
+    puts("Cannot run get_rgb.lua file!");
+	return -1;
+  }
+  
+  //=============================================
+  unsigned char red, green, blue;
+  lua_getglobal(L, "background");
+  if (lua_isstring(L, -1)) { /* value is a string? */
+    const char *colorname = lua_tostring(L, -1); /* get string */
+    int i; /* search the color table */
+	for (i = 0; colortable[i].name != NULL; i++) {
+	  if (strcmp(colorname, colortable[i].name) == 0){
+	    break;
+	  }
+	}
+	
+	if (colortable[i].name == NULL){ /* string not found? */
+	  error(L, "invalid color name (%s)", colorname);
+	}else { /* use colortable[i] */
+	  red = colortable[i].red;
+	  green = colortable[i].green;
+	  blue = colortable[i].blue;
+	  printf("%d,%d,%d", red, green, blue);
+	}
+  } else if (lua_istable(L, -1)) {
+    red = getfield(L, "r");
+	green = getfield(L, "g");
+	blue = getfield(L, "b");
+  } else{
+	  error(L, "invalid value for 'background'");
+  }
+  //=============================================
 
-  //const char *fname = "config.lua";
   lua_close(L);
   return 0;
 }
